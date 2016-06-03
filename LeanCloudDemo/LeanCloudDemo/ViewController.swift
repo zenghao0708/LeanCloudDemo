cc83c079e91d9055ed9b3ad0983a24bb23af2d24
zenghao0708 <zenghao0708@gmail.com>
2016-06-03 16:24:48 +0800

    init repo

---
+++ LeanCloudDemo/LeanCloudDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LeanCloudDemo
+//
+//  Created by Howie Zeng on 16/6/3.
+//  Copyright © 2016年 zenghao0708. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
